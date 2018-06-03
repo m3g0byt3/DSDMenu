1227c269892a56d19e09c30a4deca15ffd6e63ee
m3g0byt3 <m3g0byt3@gmail.com>
2018-06-03 22:22:48 +0300

    VC as DropDownMenu’ delegate in example

--- DropDownButton/DropDownDelegate.swift
+++ DropDownButton/DropDownDelegate.swift
@@ -8,29 +8,53 @@
 
 import Foundation
 
-@objc
-public protocol DropDownDelegate: NSObjectProtocol {
-    
+public protocol DropDownDelegate: class {
+
     /**
      Required method, provides number of items in menu
      - parameter DropDownMenu: DropDownMenu instance
      - returns: number of items in menu
      */
     func numberOfItems(in dropDownMenu: DropDownMenu) -> Int
-    
+
+    /**
+     Optional method, returns type of cell used in DropDownMenu.
+
+     Default implementation returns `DropDownCell.self`.
+     - parameter dropDownMenu: DropDownMenu instance
+     */
+    func cellClass(for dropDownMenu: DropDownMenu) -> DropDownCell.Type
+
     /**
      Optional method, called when user selects item in menu.
-     
+
      Default implementation does nothing.
      - parameters:
-     - dropDownMenu: DropDownMenu instance
-     - index: Index
+        - dropDownMenu: DropDownMenu instance
+        - index: Index
      */
     func dropDownMenu(_ dropDownMenu: DropDownMenu, didSelectItemAt index: Int)
+
+    /**
+     Optional method, called when menu is ready to display a cell.
+
+     Default implementation does nothing.
+     - parameters:
+        - dropDownMenu: DropDownMenu instance
+        - cell: DropDownCell instance
+        - index: Index
+     */
+    func dropDownMenu(_ dropDownMenu: DropDownMenu, willDisplay cell: DropDownCell, forRowAt index: Int)
 }
 
 extension DropDownDelegate {
-    
-    // Default implementation for `dropDownMenu(_:didSelectItemAt:), does nothing`
+
+    // Default implementation for `cellClass(for:)`, returns `DropDownCell.self`.
+    func cellClass(for dropDownMenu: DropDownMenu) -> DropDownCell.Type { return DropDownCell.self }
+
+    // Default implementation for `dropDownMenu(_:didSelectItemAt:)`, does nothing.
     func dropDownMenu(_ dropDownMenu: DropDownMenu, didSelectItemAt index: Int) {}
+
+    // Default implementation for `dropDownMenu(_:willDisplay:forRowAt:), does nothing.`
+    func dropDownMenu(_ dropDownMenu: DropDownMenu, willDisplay cell: DropDownCell, forRowAt index: Int) {}
 }
--- DropDownButton/ViewController.swift
+++ DropDownButton/ViewController.swift
@@ -12,11 +12,21 @@ class ViewController: UIViewController {
     
     // MARK: - IBOutlets
     
-    @IBOutlet private weak var dropDown: DropDownMenu!
+    @IBOutlet private weak var dropDownWithNib: DropDownMenu! {
+        didSet {
+            dropDownWithNib.delegate = self
+        }
+    }
+    
+    @IBOutlet private weak var dropDownWithoutNib: DropDownMenu! {
+        didSet {
+            dropDownWithoutNib.delegate = self
+        }
+    }
     
     // MARK: - Properties
     
-    private let model = [
+    private let models = [
         Fruit(name: "Apple", image: #imageLiteral(resourceName: "apple")),
         Fruit(name: "Pineapple", image: #imageLiteral(resourceName: "pineapple")),
         Fruit(name: "Lemon", image: #imageLiteral(resourceName: "lemon")),
@@ -24,4 +34,34 @@ class ViewController: UIViewController {
         Fruit(name: "Raspberry", image: #imageLiteral(resourceName: "raspberry")),
         Fruit(name: "Orange", image: #imageLiteral(resourceName: "orange")),
         ]
+}
+
+// MARK: - DropDownDelegate protocol conformance
+
+extension ViewController: DropDownDelegate {
+    
+    func cellClass(for dropDownMenu: DropDownMenu) -> DropDownCell.Type {
+        
+        switch dropDownMenu {
+        case dropDownWithNib: return CustomDropDownCellWithNib.self
+        case dropDownWithoutNib: return CustomDropDownCellWithoutNib.self
+        default: preconditionFailure("Unknown DropDownMenu instance.")
+        }
+    }
+    
+    func dropDownMenu(_ dropDownMenu: DropDownMenu, willDisplay cell: DropDownCell, forRowAt index: Int) {
+        let model = models[index]
+        guard let customCell = cell as? CustomDropDownCellWithNib else { return }
+        
+        customCell.configureUsing(model)
+    }
+    
+    func numberOfItems(in dropDownMenu: DropDownMenu) -> Int {
+        
+        switch dropDownMenu {
+        case dropDownWithNib: return models.count
+        case dropDownWithoutNib: return 3
+        default: preconditionFailure("Unknown DropDownMenu instance.")
+        }
+    }
 }
